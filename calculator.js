42ef08dcf0af208023ace6f3eb47a9610d259647
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-17 15:42:57 +0900

    Feat: Add functions for the 4 basic arithmetic operations

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+console.log(add(2,5));
+console.log(subtract(10,17));
+console.log(multiply(4,22));
+console.log(divide(100,3));
